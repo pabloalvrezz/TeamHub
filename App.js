70824ac8752447e9176c9ba18b5e15aece08b56d
pabloalvrezz <pabloalvarezgonzalez4190@gmail.com>
2024-04-03 13:49:16 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import UserList from './screens/UserList';
+import UserDetailScreen from './screens/UserScreenDetail';
+import CreateUserScreen from './screens/CreateUserScreen';
+
+const stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <stack.Navigator>
+      <stack.Screen name="CreateUserScreen" component={CreateUserScreen}/>
+      <stack.Screen name="UserListScreen" component={UserList}/>
+      <stack.Screen name="UserDetailScreen" component={UserDetailScreen}/>
+    </stack.Navigator>
+  );
+
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/UserList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native-web";
+
+const UsersList = () => {
+  return (
+    <View>
+      <Text>Users List</Text>
+    </View>
+  );
+};
+
+export default UsersList;
